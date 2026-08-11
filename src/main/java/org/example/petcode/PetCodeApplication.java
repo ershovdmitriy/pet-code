e72889c9710553abebd3ec9f8c9f16213f7aa51d
Ershov Dmitriy <yershovdmitriyalex@gmail.com>
2026-08-11 16:29:49 +0500

    chore: initial project structure with Spring Boot 4

---
+++ src/main/java/org/example/petcode/PetCodeApplication.java
@@ -0,0 +1,13 @@
+package org.example.petcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/petcode/PetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.petcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
